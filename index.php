e981ac87d66818d4e98a974613a04f64778b2419
lhuq <lhuuuq@gmail.com>
2017-03-17 09:37:41 +0100

    Separate function Selector parameter

--- index.php
+++ index.php
@@ -8,18 +8,23 @@
 </head>
 <body>
 
+<h1>Clock Transition Demo</h1>
 
-<div id='gallery' class='clock-transition'>
+<div id='photos' class='ct'>
 	<img src='images/camille-minouflet-111751.jpg'/>
 	<img src='images/edward-franklin-72304.jpg'>
 	<img src='images/nina-strehl-140747.jpg'>
 </div>
 
+<div id='food' class='ct'>
+	<img src='images/alex-munsell-18753.jpg'/>
+	<img src='images/geo-darwin-39403.jpg'/>
+	<img src='images/lauren-lester-205923.jpg'/>
+	<img src='images/yanko-peyankov-49164.jpg'/>
 
+</div>
 
-
-<script type='text/javascript' src='js/jquery.js'></script>
-<script type='text/javascript' src='js/jquery-ui.js'></script>
+<script type='text/javascript' src='js/clockTransition.js'></script>
 <script type='text/javascript' src='js/main.js'></script>
 </body>
 </html>
